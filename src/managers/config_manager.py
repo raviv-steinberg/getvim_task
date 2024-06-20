4453ede3f04a15fdb5bc88f04fe8656d263acc45
raviv <raviv.steinberg@gmail.com>
2024-06-20 18:18:00 +0300

    Minor.

--- src/managers/config_manager.py
+++ src/managers/config_manager.py
@@ -6,15 +6,28 @@ FILE_NAME = 'config.yaml'
 
 
 class ConfigManager:
+    """
+    Manages the configuration settings from a YAML file.
+    """
+
     def __init__(self):
-        root = os.path.join(Project.get_rootpath(), FILE_NAME)
+        """
+        Initializes the ConfigManager by setting the path to the configuration file and loading its content.
+        """
         self.config_file = os.path.join(Project.get_rootpath(), FILE_NAME)
         self.config = self._load_config()
 
-    def _load_config(self):
+    def _load_config(self) -> dict:
+        """
+        Loads the configuration from the YAML file.
+        :return: The loaded configuration data
+        """
         with open(self.config_file, 'r') as file:
             return yaml.safe_load(file)
 
     def get_url(self) -> str:
+        """
+        Retrieves the URL from the configuration settings.
+        :return: The URL from the configuration settings, or an empty string if not found
+        """
         return self.config.get('settings', {}).get('url', '')
-
